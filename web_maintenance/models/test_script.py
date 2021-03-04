a6e9173edfffca41fc648b453cd95d646390551c
msaid251 <mdssad3@gmail.com>
2021-03-05 00:18:13 +0200

    [ِADD] web_maintenance

--- web_maintenance/models/test_script.py
+++ web_maintenance/models/test_script.py
@@ -1,28 +1,35 @@
 import requests
 import json
+# url_connect = "http://localhost:8014/web/session/authenticate"
+# url = "http://localhost:8014/machine/info/"
+# data_connect = {
+#     "params": {
+#         "db": "fields2",
+#         "login": "admin",
+#         "password": "1",
+#     }
+# }
 
-# url_connect = "http://75.119.131.24/web/session/authenticate"
-url_connect = "http://localhost:8014/web/session/authenticate"
-# url = "http://75.119.131.24/machine/info/"
-url = "http://localhost:8014/machine/info/"
+# target server authenticate url
+
+url_connect = "http://75.119.131.24/web/session/authenticate"
+
+# url to create data coming form skada
+
+url = "http://75.119.131.24/machine/info/"
 
 headers = {'Content-Type': 'application/json'}
+# data to connect with database
 
 data_connect = {
     "params": {
-        "db": "fields2",
+        "db": "ps_group",
         "login": "admin",
-        "password": "1",
+        "password": "admin",
     }
 }
-#
-# data_connect = {
-#     "params": {
-#         "db": "ps_group",
-#         "login": "admin",
-#         "password": "admin",
-#     }
-# }
+
+# please send data like this format
 
 data = [
     {
@@ -39,17 +46,20 @@ data = [
         'BAMS_Mac_Code': '201102',
         'BAMS_Mac_Desc': 'Nail Machine #2',
     }]
-
+# create new session
 session = requests.Session()
-
-r = session.post(url=url_connect, data=json.dumps(data_connect), headers=headers)
-
-if r.ok:
-    result = r.json()['result']
-
+connect_session = session.post(url=url_connect, data=json.dumps(data_connect), headers=headers)
+if connect_session.ok:
+    result = connect_session.json()['result']
     if result.get('session_id'):
         session.cookies['session_id'] = result.get('session_id')
 
-r = session.post(url=url, data=json.dumps({'data': data}), headers=headers)
-print(r)
-print(r.json())
+# ----------------------------
+
+# post data from skada to odoo to create it
+
+response = session.post(url=url, data=json.dumps({'data': data}), headers=headers)
+print(response)
+
+# response.json()  = {'jsonrpc': '2.0', 'id': None, 'result': '{"success": [29, 30]}'}
+print(response.json())
