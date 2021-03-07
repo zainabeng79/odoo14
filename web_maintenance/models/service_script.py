565729653cc3889dfacdf9cf55cf9d02e0ddcdd2
msaid <m.said@itqansystems.com>
2021-03-08 00:18:37 +0200

    update

--- web_maintenance/models/service_script.py
+++ web_maintenance/models/service_script.py
@@ -27,28 +27,43 @@ session = requests.Session()
 connect_session = session.post(url=url_connect, data=json.dumps(data_connect), headers=headers)
 if connect_session.ok:
     result = connect_session.json()['result']
-    if result.get('session_id'):
-        session.cookies['session_id'] = result.get('session_id')
+    if result.get('ocn_token_key'):
+        session.cookies['session_id'] = result.get('ocn_token_key')
 # ----------------------------
 
 # url to create data coming form skada
 url = "http://75.119.131.24/machine/info/"
 # please send data like this format
 data = [
-    {
+    (0,0,{
         'Machine_Code': '232100',
         'Machine_Name': 'NAIL # 1 Wafios N-41',
         'Machine_ShortName': 'NL-01',
         'BAMS_Mac_Code': '201101',
         'BAMS_Mac_Desc': 'Nail Machine #1',
-    },
-    {
+    }),
+    (0,0,{
         'Machine_Code': '232101',
         'Machine_Name': 'NAIL # 1 Wafios N-41',
         'Machine_ShortName': 'NL-02',
         'BAMS_Mac_Code': '201102',
         'BAMS_Mac_Desc': 'Nail Machine #2',
-    }]
+    }),
+    (0,0,{
+        'Machine_Code': '232102',
+        'Machine_Name': 'NAIL # 1 Wafios N-42',
+        'Machine_ShortName': 'NL-03',
+        'BAMS_Mac_Code': '201103',
+        'BAMS_Mac_Desc': 'Nail Machine #4',
+    }),
+    (0,0,{
+        'Machine_Code': '232103',
+        'Machine_Name': 'NAIL # 1 Wafios N-42',
+        'Machine_ShortName': 'NL-03',
+        'BAMS_Mac_Code': '201103',
+        'BAMS_Mac_Desc': 'Nail Machine #3',
+    }),
+]
 # post data from skada to odoo to create it
 response = session.post(url=url, data=json.dumps({'data': data}), headers=headers)
 
