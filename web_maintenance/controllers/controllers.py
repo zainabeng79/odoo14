3526a36b26c1387736d6f8a346ae6af79d2d2ad9
msaid <m.said@itqansystems.com>
2021-03-08 00:24:28 +0200

    update

--- web_maintenance/controllers/controllers.py
+++ web_maintenance/controllers/controllers.py
@@ -57,6 +57,6 @@ class MachineInfo(http.Controller):
         except Exception as e:
             return e
         try:
-            return json.dumps({"success": "hghghghg"})
+            return json.dumps({"success": ifo_ids})
         except Exception as e:
             return e
